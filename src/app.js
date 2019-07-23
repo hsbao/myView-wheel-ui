8a76d7d4715f485afa41e7e025eaac5f694571d0
Abao <771989536@qq.com>
2019-07-23 20:33:39 +0800

    添加parcel, 完成基本Button组件样式

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import Button from './components/Button.vue'
+
+new Vue({
+  el: '#app',
+  components: {
+    'mview-button': Button
+  }
+})
---
+++ src/components/Button.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="view-button">按钮</button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.view-button {
+  height: var(--button-height);
+  font-size: var(--font-size);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  outline: none;
+
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+
+  &:active { 
+    background: var(--button-active-bg);
+  }
+
+  &:focus {
+    outline: none;
+  }
+}
+</style>
