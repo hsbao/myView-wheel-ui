128211ed1b20a2fe8684332310131ed512af87d6
Abao <771989536@qq.com>
2019-07-26 21:15:11 +0800

    修改package.json

---
+++ index.js
@@ -0,0 +1,9 @@
+import Button from './src/components/Button'
+import ButtonGroup from './src/components/Button-Group'
+import Icon from './src/components/Icon'
+
+export {
+  Icon,
+  Button,
+  ButtonGroup
+}
